6e43a826bc1061e53c2f332e4fb5cd5e0b3d1b1a
wkimak <wkimak94@gmail.com>
2018-11-02 14:13:09 -0400

    edit

--- server/database/index.js
+++ server/database/index.js
@@ -4,11 +4,11 @@ console.log(database.mysqlEndpoint)
 const knex = require('knex')({
   client: 'mysql',
   connection: {
-    host: 's54ham9zz83czkff.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-    username: 'kycoslt1dbsc9dyb',
-    password: ' o86xs5dheasyy6os',
+    host: 'tk3mehkfmmrhjg0b.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    username: 'giuz7rbdus19wida',
+    password: ' nhz9xzta67xzqj0m',
     port: 3306,
-    database: 'aja1irdaehd1rwng'
+    database: 'o4sf9s7p7kweylux'
   }
 });
 
