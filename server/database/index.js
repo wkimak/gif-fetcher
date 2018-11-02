389703eb5f92a6cb6393f4ebf4a8bbe6a7a9ff03
wkimak <wkimak94@gmail.com>
2018-11-02 13:43:19 -0400

    aedit

--- server/database/index.js
+++ server/database/index.js
@@ -4,7 +4,7 @@ console.log(database.mysqlEndpoint)
 const knex = require('knex')({
   client: 'mysql',
   connection: {
-    host: 's54ham9zz83czkff.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    host: 'mysql://kycoslt1dbsc9dyb:o86xs5dheasyy6os@s54ham9zz83czkff.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/aja1irdaehd1rwng',
     username: 'kycoslt1dbsc9dyb',
     password: ' o86xs5dheasyy6os',
     port: 3306,
