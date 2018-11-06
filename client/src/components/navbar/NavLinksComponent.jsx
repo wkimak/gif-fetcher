cf191a74152eac558e9e8223b5c40c4afef694a2
wkimak <wkimak94@gmail.com>
2018-11-05 21:28:03 -0500

    minor style changes to login form

--- client/src/components/navbar/NavLinksComponent.jsx
+++ client/src/components/navbar/NavLinksComponent.jsx
@@ -4,27 +4,32 @@ class NavLinks extends Component {
   
   state={ linkSelected: 0 }
 
-  handleSelect = (value) => {
-    const { handleSearch } = this.props;
-     if(value === 0) {
-       handleSearch('trending', null, 12, 0);
-     } else if(value === 1) {
-       handleSearch('search', 'entertainment', 12, 0);
-     } else if(value === 2) {
-       handleSearch('search', 'sports', 12, 0);
-     } else if(value === 3) {
-       handleSearch('search', 'politics', 12, 0);
-     } else {
-       handleSearch('translate', null, 12, 0);
-     }
+  handleSelect = (e) => {
+    const value = e.target.value, 
+          tagName = e.target.tagName,
+          { handleSearch } = this.props;
+
+     if(tagName === 'LI') {
+       if(value === 0) {
+         handleSearch('trending', null, 12, 0);
+       } else if(value === 1) {
+         handleSearch('search', 'entertainment', 12, 0);
+       } else if(value === 2) {
+         handleSearch('search', 'sports', 12, 0);
+       } else if(value === 3) {
+         handleSearch('search', 'politics', 12, 0);
+       } else if(value === 4) {
+         handleSearch('translate', null, 12, 0);
+       }
 
      this.setState({ linkSelected: value });
+   }
   }
 
   render() {
     const { linkSelected } = this.state;
     return (
-      <ul className='nav_links_ul' onClick={ (e) => this.handleSelect(e.target.value) }>
+      <ul className='nav_links_ul' onClick={ (e) => this.handleSelect(e) }>
         <li value={0} className={ linkSelected === 0 ? 'link_active' : null }>Trending</li>
         <li value={1} className={ linkSelected === 1 ? 'link_active' : null }>Entertainment</li>
         <li value={2} className={ linkSelected === 2 ? 'link_active' : null }>Sports</li>
